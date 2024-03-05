9541c8e88ae2bcac1ac5a865ea94c61005ae61ce
Ram Verma <ram@Rams-MacBook-Pro.local>
2024-03-05 11:52:34 +0530

    exchange connector setup

---
+++ src/wsClient.ts
@@ -0,0 +1,59 @@
+import WebSocket from 'ws'
+import { AuthenticationError } from './error'
+
+export class WsClient {
+  ws: WebSocket
+
+  constructor(
+    type: 'public' | 'private',
+    option: 'mainnet' | 'testnet' = 'mainnet',
+    jwt?: string | null,
+  ) {
+    let connection = ''
+    const baseUrl =
+      option === 'testnet' ? 'wss://testnet-exchange-api.buckmint.org' : 'wss://exchange-api.buckmint.org'
+    if (type === 'public') connection = `${baseUrl}/public`
+    else {
+      if (!jwt)
+        throw new AuthenticationError(
+          'JWT access token must be provided for private connections',
+        )
+      connection = `${baseUrl}/private?auth_header=${jwt}`
+    }
+    this.ws = new WebSocket(connection)
+  }
+
+  async connect(): Promise<void> {
+    while (this.ws.readyState === 0) {
+      await this.sleep(500)
+    }
+  }
+
+  async disconnect(): Promise<void> {
+    this.ws.close()
+  }
+
+  async sleep(ms: number): Promise<unknown> {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms)
+    })
+  }
+
+  async subscribe(streams: string[]): Promise<void> {
+    const msg = {
+      event: 'subscribe',
+      streams,
+    }
+
+    this.ws.send(JSON.stringify(msg))
+  }
+
+  async unsubscribe(streams: string[]): Promise<void> {
+    const msg = {
+      event: 'unsubscribe',
+      streams,
+    }
+
+    this.ws.send(JSON.stringify(msg))
+  }
+}
